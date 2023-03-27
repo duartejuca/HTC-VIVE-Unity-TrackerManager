5519fb51542e11dd257a9f09a6f8df41fbbda0f3
Juca Duarte <5769936+duartejuca@users.noreply.github.com>
2023-03-27 18:54:27 -0300

    Update Tracker
    Cleaning code. But still some parts to refactor.

--- TrackerManager.cs
+++ TrackerManager.cs
@@ -1,197 +1,135 @@
-/*
-* DEVELOPED BY: JOAO MANOEL FRANÇA DUARTE BONGIOVANI (JOAO DUARTE)
-* ESTA FERRAMENTA DEVE SER UTILIZADA PARA DESCOBERTA DE DADOS RELACIONADOS AO TRACKER DA HTC
-*/
-
+// DESENVOLVIMENTO: JOAO MANOEL FRANÇA DUARTE BONGIOVANI (João Duarte)
+// V1.0 
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using UnityEditor;
 using UnityEngine;
 using Valve.VR;
 using static Valve.VR.SteamVR_TrackedObject;
 
-namespace ErgoTraining.Inputs
+namespace Tracker.Inputs
 {
-    [System.Serializable]
-    public struct HardwareDeviceProp
-    {
-        public string deviceModel;
-        public bool isUsed;
 
-        public HardwareDeviceProp(string deviceModel, bool isUsed)
+public enum TrackerPart { LeftShoulder, RightShoulder, LeftFoot, RightFoot, Waist }
+    
+    [Serializable]
+    public struct VirtualDevice
+    {
+        public EIndex DeviceIndex;
+        public string Limb;
+
+        public VirtualDevice(EIndex deviceIndex, string limb)
         {
-            this.deviceModel = deviceModel;
-            this.isUsed = isUsed;
+            DeviceIndex = deviceIndex;
+            Limb = limb;
+        }
+    }
+    
+    
+
+    [Serializable]
+    public struct TrackerLocal
+    {
+        public SteamVR_TrackedObject TrackerObject;
+        public TrackerPart Limb;
+
+        public TrackerLocal(SteamVR_TrackedObject deviceIndex, TrackerPart Limb)
+        {
+            TrackerObject = deviceIndex;
+            this.Limb = Limb;
         }
     }
 
-    public class TrackerManager : MonoBehaviour
+    public sealed class TrackerManager : MonoBehaviour
     {
-
         private static TrackerManager instance;
         public static TrackerManager Instance { get { return instance; } private set { } }
+        private const ETrackedDeviceProperty _config = ETrackedDeviceProperty.Prop_ControllerType_String;
+        [SerializeField] private int MaxHadwareDevicesConnected = 20;
+        private List<VirtualDevice> VirtualDevices;
 
-        private void Awake()
-        {
-            instance = this;
-        }
-
-        /// <summary>Número de trackers a serem listados.</summary>
-        public int VirtualDevicesToUse = 5;
-
-
-        [SerializeField] private int MaxHadwareDevices = 20;
-        [SerializeField] private List<EIndex> VirtualDevices;
-        private List<HardwareDeviceProp> HardwareDevices = new List<HardwareDeviceProp>();
-
-        /// <summary>Retorna a quantidade de dispositivos conectados e ativos.</summary>
-        public int GetDevicesConected() => VirtualDevices.Count;
+        [SerializeField] private TrackerLocal[] LocalTrackers;
         
-        
-        private static List<T> GetEnumList<T>()
-        {
-            T[] array = (T[])Enum.GetValues(typeof(T));
-            List<T> list = new List<T>(array);
-            return list;
-        }
+
+        private void Awake() => instance = this;
 
         private void Start()
         {
-            
-            StartCoroutine(DeviceCheck());
+            FillDevices();
         }
 
-        public ETrackedDeviceProperty RoleToReturn;
-        
-        // nome do tracker na configuração do STEAMVR [retorna ex: LHR-E54F8FFB]
-        const ETrackedDeviceProperty firmwareName = ETrackedDeviceProperty.Prop_Firmware_ProgrammingTarget_String;
-        const ETrackedDeviceProperty limbConfig = ETrackedDeviceProperty.Prop_ControllerType_String;
-
-        private void FindTrackerHardwareDevices()
+        public List<string> FindAllRegisteredDevices()
         {
-            Debug.Log("[ERGOTRAINING]: Verificando dispositivos conectados.");
+            UnityEngine.Debug.Log("Verificying connected devices.");
             ETrackedPropertyError error = ETrackedPropertyError.TrackedProp_Success;
-            
             StringBuilder id = new(64);
-            StringBuilder id2 = new(64);
+            List<string> harddevices = new List<string>();
 
-            for (int hard = 0; hard < MaxHadwareDevices; hard++)
+            for (int hard = 0; hard < MaxHadwareDevicesConnected; hard++)
             {
-                try {
-                    // retornando todos os hardwares conectados da VIVE.
-                    //OpenVR.System.GetStringTrackedDeviceProperty((uint)hard, ETrackedDeviceProperty.Prop_RenderModelName_String, id, 64, ref error);
-                    OpenVR.System.GetStringTrackedDeviceProperty((uint)hard, limbConfig, id, 64, ref error);
-                    OpenVR.System.GetStringTrackedDeviceProperty((uint)hard, RoleToReturn, id2, 64, ref error);
-
-                   
-                }
-                catch (Exception e)
-                {
-                    Debug.LogError($"[ERGOTRAINING]: Não foi encontrado nenhum dispositivo de realidade virtual: {e.Message} | {e.Source}");
-                    return;
-                }
-                
-                if(id2.ToString() != string.Empty)
-                    Debug.Log($"Dispositivo Conectado ID2: {id2}");
-
+                OpenVR.System.GetStringTrackedDeviceProperty((uint)hard, _config, id, 64, ref error);
                 string device = id.ToString();
-                if (device != string.Empty) { HardwareDevices.Add(new HardwareDeviceProp(device, false)); }
-            }
-        }
-
-        // RETORNA TODAS AS INFORMAÇÕES DO PRODUTO
-        private void AllDevicesConnectedInfo()
-        {
-            List<ETrackedDeviceProperty> ETrackDevice = new List<ETrackedDeviceProperty>();
-            ETrackDevice = GetEnumList<ETrackedDeviceProperty>();
-            ETrackedPropertyError error = ETrackedPropertyError.TrackedProp_Success;
-            StringBuilder id = new(64);
-
-            for (int hard = 0; hard < MaxHadwareDevices; hard++)
-            {
-                foreach (var device in ETrackDevice)
+                if (device != string.Empty) 
                 {
-                    OpenVR.System.GetStringTrackedDeviceProperty((uint)hard, device, id, 64, ref error);
-                    Debug.Log($"{ETrackDevice}: {id}");
+                    harddevices.Add(new string(device)); 
                 }
             }
+            return harddevices;
         }
 
-        private bool HadwareToVirtualDevices()
+        public void FillDevices()
         {
-            for (int hard = 0; hard < HardwareDevices.Count; hard++)
+            List<string> harddevices = FindAllRegisteredDevices();
+            VirtualDevices.Clear();
+            for (int hard = 0; hard < harddevices.Count; hard++)
             {
-                if (HardwareDevices[hard].deviceModel.Contains("tracker"))
+                if (harddevices[hard].Contains("tracker"))
                 {
-                    if (!HardwareDevices[hard].isUsed)
+                    VirtualDevice vd = new VirtualDevice((EIndex)hard, harddevices[hard]);
+                    VirtualDevices.Add(vd);
+                    continue;
+                }
+            }
+
+            AttachVirtualToModule(TrackerPart.RightShoulder);
+            AttachVirtualToModule(TrackerPart.LeftShoulder) ;
+            AttachVirtualToModule(TrackerPart.LeftFoot);
+            AttachVirtualToModule(TrackerPart.RightFoot);
+            AttachVirtualToModule(TrackerPart.Waist);
+        }
+
+        private string TrackerPartToString(TrackerPart TrackerModule)
+        {
+            string part = "";
+            switch (TrackerModule)
+            {
+                case TrackerPart.RightShoulder: part = "right_shoulder"; break;
+                case TrackerPart.LeftShoulder: part = "left_shoulder"; break;
+                case TrackerPart.LeftFoot: part = "left_foot"; break;
+                case TrackerPart.RightFoot: part = "right_foot"; break;
+                case TrackerPart.Waist: part = "waist"; break;
+            }
+            return part;
+        }
+
+        private void AttachVirtualToModule(TrackerPart TrackerModule)
+        {
+            foreach(VirtualDevice vd in VirtualDevices)
+            {
+                if (vd.Limb.Contains(TrackerPartToString(TrackerModule)))
+                {
+                    foreach (TrackerLocal lt in LocalTrackers)
                     {
-                        VirtualDevices.Add((EIndex)hard);
-                        HardwareDevices[hard] = new HardwareDeviceProp(HardwareDevices[hard].deviceModel, true);
-                        continue;
+                        if(lt.Limb == TrackerModule)
+                        {
+                            lt.TrackerObject.index = vd.DeviceIndex;
+                        }
                     }
                 }
+                continue;
             }
-
-            if (RegistryTrackersToUse()) return true;
-            else return false;
         }
-
-        /// <summary>Troca a configuração ativa do Tracker indicado no parametro Index.</summary>
-        public void VirtualTrackersConfig(SteamVR_TrackedObject VirtualTracker, int Index) => VirtualTracker.index = VirtualDevices[Index];
-
-        private bool RegistryTrackersToUse()
-        {
-            if (VirtualDevices.Count < VirtualDevicesToUse)
-            {
-                Debug.LogError($"[ERGOTRAINING]: Número de dispostitivos irregular, necessário {VirtualDevicesToUse} dispositivos conectados.");
-                return false;
-            }
-
-            return true;
-        }
-
-        
-
-        
-
-        private IEnumerator DeviceCheck()
-        {
-            
-
-            FindTrackerHardwareDevices();
-            yield return new WaitForSeconds(2);
-
-            yield return new WaitUntil(() => HadwareToVirtualDevices());
-
-            //FindTrackerHardwareDevices()
-            Debug.Log("Registrado");
-
-
-            //DefaultVRTrackersSetup();
-
-            yield return null;
-            yield break;
-        }
-
-        /// <summary>Aplica o tracker por meio da tela de configuração.</summary>
-       /* public void SetTrackerToGameObject(int TrackerIndex)
-        {
-            if (TrackerIndex > GameManager.Instance.PlayerTrackers.Count && TrackerIndex > VirtualDevices.Count)
-            { Debug.LogError("[ERGOTRAINING]: Indice selecionado inexistente."); return; }
-
-            VirtualTrackersConfig(GameManager.Instance.PlayerTrackers[TrackerIndex].GetComponent<SteamVR_TrackedObject>(), TrackerIndex);
-        }
-
-
-        /// <summary>Aplica o tracker com configuração default.</summary>
-        public void DefaultVRTrackersSetup()
-        {
-            for (int i = 0; i < VirtualDevicesToUse; i++)
-            {
-                VirtualTrackersConfig(GameManager.Instance.PlayerTrackers[i].GetComponent<SteamVR_TrackedObject>(), i);
-                Debug.Log("[ERGOTRAINING]: Dispositivos encontrados e configurados.");
-            }
-        }*/
     }
 }
